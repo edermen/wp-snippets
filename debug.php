822bceee4852620a739cb81fb545cb2e2461f2ba
solli86 <paul-sart@yandex.ru>
2016-05-27 11:36:24 +0300

    initial commit

---
+++ debug.php
@@ -0,0 +1,11 @@
+<?php 
+// ==== DEBUG ====
+  // load global styles array
+  global $wp_styles;
+  var_dump($wp_styles);
+
+  // load global scripts array
+  global $wp_scripts;
+  var_dump($wp_scripts);
+// EN DEBUG
+?>
